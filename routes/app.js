eef38148c0b9f634e6cfd48f8e1c57982df615e9
Ignat Ignatov <ignat99@gmail.com>
2014-04-09 18:40:12 +0200

    [FEATURE] Add routes and right path.

---
+++ routes/app.js
@@ -0,0 +1,47 @@
+ 
+/**
+ * Module dependencies.
+ */
+
+
+var express = require('express')
+  , routesPayments = require("./payments")
+  , routesApi = require("./api")
+  , routesIndexIndex = require("./index")
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+
+app.get('/', routesIndexIndex.index);
+app.post('/payments/:id', routesPayments.notification);
+
+// JSON API
+app.get('/switches', routesApi.switches);
+app.get('/switches/:id', routesApi.switch);
+app.post('/switches', routesApi.addSwitch);
+app.put('/switches/:id', routesApi.editSwitch);
+app.delete('/switches/:id', routesApi.deleteSwitch);
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
